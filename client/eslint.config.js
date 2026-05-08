0ce023ecc21a66c0a2f4e2247f7942f7dc90de8b
Hadree <hadree.mustaffa@gmail.com>
2026-05-08 17:01:56 +0800

    install vite and react

---
+++ client/eslint.config.js
@@ -0,0 +1,21 @@
+// client/eslint.config.js
+
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import baseConfig from '../eslint.config.js';
+
+export default [
+  ...baseConfig,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
+];
