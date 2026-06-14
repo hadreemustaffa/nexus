b69a947541697c3aa90e3520dbc73df087675daf
Hadree <hadree.mustaffa@gmail.com>
2026-06-14 17:43:44 +0800

    feat(frontend): show proper error message for root error boundary

--- client/src/app/routes/app/Root.tsx
+++ client/src/app/routes/app/Root.tsx
@@ -1,14 +1,47 @@
-import { Outlet, useLocation, useMatch } from 'react-router';
+import {
+  isRouteErrorResponse,
+  Outlet,
+  useLocation,
+  useMatch,
+  useNavigate,
+  useRouteError,
+} from 'react-router';
 
 import { paths } from '../../../config/paths';
 import { ERRORS } from '../../../shared';
+import Button from '../../../shared/ui/button/Button';
 import AppLayout from '../../../shared/ui/layouts/AppLayout';
 import NotesLayout from '../../../shared/ui/layouts/NotesLayout';
 import Loader from '../../../shared/ui/loader/Loader';
 import styles from './Root.module.css';
 
+function ErrorElement({ message }: { message: string }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.error_container}>
+      <div className={styles.error}>
+        <div>{message}</div>
+        <Button onClick={() => navigate(-1)}>Go back</Button>
+      </div>
+    </div>
+  );
+}
+
 export const AppRootErrorBoundary = () => {
-  return <div className={styles.error}>{ERRORS.API.SERVER_ERROR}</div>;
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      return <ErrorElement message={ERRORS.API.NOT_FOUND} />;
+    }
+
+    if (error.status === 500) {
+      return <ErrorElement message={ERRORS.SERVER_ERROR} />;
+    }
+  }
+
+  return <ErrorElement message={ERRORS.NETWORK_ERROR} />;
 };
 
 export const AppRootLoader = () => {
